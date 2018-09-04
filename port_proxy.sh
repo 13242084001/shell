3b62443ff91bbae9aafa1a8744a6a83c87484ac8
13242084001 <13242084001@163.com>
2018-09-04 22:03:37 +0800

    新增expect自动登陆远程主机执行shell

--- port_proxy.sh
+++ port_proxy.sh
@@ -15,13 +15,19 @@ ip route add table 10 via 183.xxx.xx.70 dev p2p2
 ip rule fwmark 10 table 10
 
 #服务器A设置，收到服务器B的443报文后，报文的源ip地址仍是B的地址183.xxx.xx.70,然后根据目的地址查询路由表转发出去，下一跳是网关，报文回来的时候，我们也希望经过服务器A，然而现在报文的源地址是仍是B的地址，网关会直接把报文转发给B，所以在A转发之前就要把报文的源地址改成自己的地址
-ssh 183.xxx.xx.70 #我这里是免密登陆到服务器A
-
-if [ `cat /proc/sys/net/ipv4/ip_forward` -eq 0 ];then
-	echo 1 > /proc/sys/net/ipv4/ip_forward
-fi
-
-iptables -t nat -A POSTROUTING -s 183.xxx.xx.71 -p tcp --dport 443 -j SNAT --to 183.xxx.xx.70
+passwd=123456
+/bin/expect <<-EOF
+spawn ssh -p 22051 root@183.xxx.xx.70
+set time 5
+expect "*password*"
+send "$passwd\n"
+expext "*#"
+send "echo 1 > /proc/sys/net/ipv4/ip_forward\n"
+expect "*#"
+send "iptables -t nat -A POSTROUTING -s 183.xxx.xx.71 -p tcp --dport 443 -j SNAT --to 183.xxx.xx.70\n"
+interact
+expect eof
+EOF
 
 #测试是否符合预期
 baidu_ip=`ping www.baidu.com -c 1|grep 'from'|awk '{print $4}'|cut -d: -f1`
