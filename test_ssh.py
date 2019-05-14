c91a7dcb717a5d75ae13e7f27656ac62b42fac11
root <root@rbs.unassigned-domain>
2019-05-14 11:25:33 +0800

    增加异常判断

--- test_ssh.py
+++ test_ssh.py
@@ -3,6 +3,8 @@ import yaml
 import os
 import time
 import paramiko
+from paramiko import AuthenticationException
+from paramiko.ssh_exception import NoValidConnectionsError
 import threading
 import logging
 
@@ -15,31 +17,48 @@ def readConfig():
     try:
         with open("./config.yaml", "r", encoding="utf-8") as f:
             data = f.read()
-        logging.info("读取配置文件内容:%s" % (data,))
+        #logging.info("读取配置文件内容:%s" % (data,))
     except Exception as e:
         print(str(e))
-        logging.error("没有配置文件! %s" % str(e))
+        #logging.error("没有配置文件! %s" % str(e))
         os._exit(0)
-    d = yaml.load(data, Loader=yaml.FullLoader)
+    d = yaml.load(data)
     return d
 
 def tunction(ip,username,password,cmd):
     client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #print(ip, username, password, type(ip), type(username), type(password))
-    client.connect(ip,username=username,password=password)
-    logging.info("正在登录%s..." % ip)
-    stdin,stdout,stdeer = client.exec_command(cmd)
-    logging.info("正在%s上执行命令" % ip)
+    try:
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        #print(ip, username, password, type(ip), type(username), type(password))
+        client.connect(ip,username=username,password=password)
+    except AuthenticationException:
+        logging.warning("username or password error!" % (ip,))
+        print("#"*30)
+        print("%s username or password error!" % (ip,))
+        return None
+    except NoValidConnectionsError:
+        logging.warning("%s connect time out!" % (ip,))
+        print("#"*30)
+        print("%s connect time out" % (ip,))
+        return None
+    #logging.info("正在登录%s..." %ip)
+    try:
+        stdin,stdout,stdeer = client.exec_command(cmd)
+    except AttributeError:
+        logging.warning("%s is not exists host!" % (ip,))
+        return None
+    #logging.info("正在%s上执行命令" % ip)
     #print(stdout.read())
     if not stdeer.read():
         print("#"*30)
-        print("\033[1;36;40m设备 %s 执行成功\033[0m" % (ip,))
-        logging.info("\033[1;36;40m设备 %s 执行成功, %s\033[0m" % (ip, ",".join(stdout)))
+        #print("\033[1;36;40m设备%s执行成功\033[0m" % (ip,))
+        print("%s is exec ok" % (ip,))
+        #logging.info("\033[1;36;40m设备 %s 执行成功, %s\033[0m" % (ip, ",".join(stdout)))
         #print("#"*30)
     else:
-        logging.error("\033[1;31;40m设备 %s 执行失败, %s\033[0m" % (ip, ",".join(stdeer)))
-        print("\033[1;31;40m设备 %s 执行失败\033[0m" % (ip,))
+        #logging.error("\033[1;31;40m设备%s执行失败,%s\033[0m" % (ip, ",".join(stdeer)))
+        #print("\033[1;31;40m设备%s执行失败\033[0m" % (ip,))
+        print("%s exec not ok! " % (ip,))
     client.close()
 def main(**kwargs):
     #字典是无序的
@@ -48,7 +67,7 @@ def main(**kwargs):
     username = kwargs.get("username")
     password = str(kwargs.get("password"))
     cmd = kwargs.get("cmd")
-    logging.debug("等待登录的设备列表: %s" % (ip_list,))
+    #logging.debug("等待登录的设备列表: %s" % (ip_list,))
     thread_list = []
     for ip in ip_list:
         t = threading.Thread(target = tunction,args = (ip,username,password,cmd))
