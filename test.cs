56865f2d36f08ff05d49d1da90d5a5ea6ba7b338
root <leslie>
2019-07-04 09:44:46 +0800

    ..

--- test.cs
+++
@@ -1,3 +0,0 @@
-{"method":"POST","url":"/login","input":{"username":"admin","password":"e10adc3949ba59abbe56e057f20f883e"},"output":{"code":10000,"token":"397d7c28-06d4-382d-8854-4af3f143d945","error":null}}
-{"method":"POST","url":"/login","input":{"username":"admin","password":"e10adc3949ba59abbe56e057f20f883e"},"output":{"code":10000,"token":"397d7c28-06d4-382d-8854-4af3f143d945","error":null}}
-{"method":"GET","url":"/TaskStatus!getRouteOptions","output":{"code":10000,"data":[{"id":7,"value":"5061<==>127.0.0.1:5060"}],"error":null}}
